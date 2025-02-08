4defbbf79dc7e5ae55f930358474d5a5509de799
ArnavS1102 <56074642+ArnavS1102@users.noreply.github.com>
2025-02-09 03:00:37 +0530

    Update make_dirs.py

--- make_dirs.py
+++ make_dirs.py
@@ -5,12 +5,16 @@ load_dotenv(".env")
 if __name__ == "__main__":
     md_path = os.getenv("MD_FOLDER")
     pdf_path = os.getenv("PDF_FOLDER")
+    csv_path = os.getenv("CSV_FOLDER")
 
     if not os.path.exists(md_path):
         os.mkdir(md_path)
 
     if not os.path.exists(pdf_path):
         os.mkdir(pdf_path)
+
+    if not os.path.exists(csv_path):
+        os.mkdir(csv_path)
     
 
 
