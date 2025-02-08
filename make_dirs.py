bc05d86323f11e80887259a9310a45e5fe315b8d
ArnavS1102 <56074642+ArnavS1102@users.noreply.github.com>
2025-02-09 02:28:38 +0530

    Add files via upload

---
+++ make_dirs.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+if __name__ == "__main__":
+    md_path = os.getenv("MD_FOLDER")
+    pdf_path = os.getenv("PDF_FOLDER")
+    
+    if not os.path.exists(md_path):
+        os.mkdir(md_path, exist_ok=True)
+
+    if not os.path.exists(pdf_path):
+        os.mkdir(pdf_path, exist_ok=True)
+    
+
+
